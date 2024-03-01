ee2a60c1589cadf3f577b1c78cffd1e8e4bcafe0
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:12:10 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Gantt from "./Gantt.svelte";
+  import { getData } from "./data.js";
+</script>
+
+<Gantt tasks={getData()} />
---
+++ src/Gantt.svelte
@@ -0,0 +1,20 @@
+<script>
+    import "@dhx/trial-gantt/codebase/dhtmlxgantt.css";
+    import { onMount } from "svelte";
+    import { Gantt } from "@dhx/trial-gantt";
+    
+    export let tasks;
+    
+    let container;
+    onMount(() => {
+        let gantt = Gantt.getGanttInstance();
+        gantt.init(container);
+        gantt.parse(tasks);
+
+        return () => {
+            gantt.destructor();
+        };
+    });
+</script>
+
+<div bind:this={container} style="width: 100%; height: 100%;"></div>
